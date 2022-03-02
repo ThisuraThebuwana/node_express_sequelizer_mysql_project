f6d20d08729ccc7060e32438a281b7cb21cdf19c
ThisuraThebuwana <thisura.thebuwana@gmail.com>
2022-03-02 15:28:23 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+var corOptions = {
+    origin: 'https://localhost:8081'
+}
+
+// middlewares
+
+app.use(cors(corOptions));
+
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: true }))
+
+// testing api
+
+app.get('/', (req, res) => {
+    res.json({ message: 'hello'})
+})
+
+//port
+
+const PORT = process.env.PORT || 8080
+
+// server
+
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}.`);
+})
