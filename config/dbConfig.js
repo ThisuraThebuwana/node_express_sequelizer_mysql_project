e367f88ff8f34e6366c05e025e6c1cbb6b3fa2b3
ThisuraThebuwana <thisura.thebuwana@gmail.com>
2022-03-02 16:03:55 +0530

    create dbConfigs & models

---
+++ config/dbConfig.js
@@ -0,0 +1,13 @@
+module.export = {
+    HOST: 'localhost',
+    USER: 'root',
+    PASSWORD: '19961014#Thebz',
+    DB: 'node_sequelize_api_db',
+    dialect: 'mysql',
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+}
